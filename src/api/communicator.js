48cb10450963d044538c456f6adb046fbc1c70fd
Tsumari <lucasflicky@hotmail.com>
2022-09-19 23:38:23 +0200

    unlock communicator

--- src/api/communicator.js
+++ src/api/communicator.js
@@ -9,9 +9,9 @@ const IP_VALIDATION_RGX = new RegExp("^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\.
 
 export default async function getIpData(ipv4){
     if ( !ipv4.match(IP_VALIDATION_RGX) ) return { status: "Error", msg: "Invalid IP address (must be ipv4)"};
-    //const {status,data} = await Axios.get( `${BASE_URL}/${ipv4}?access_key=${ACCESS_KEY}`);
-    const status = 200;
-    const data = ipstack.mockdata;
+    const {status,data} = await Axios.get( `${BASE_URL}/${ipv4}?access_key=${ACCESS_KEY}`);
+    //const status = 200;
+    //const data = ipstack.mockdata;
     if (status !== 200) return { status: "Error", msg: `Received error ${status} from API`};
     console.log(data)
     return {status:"OK", data};    
